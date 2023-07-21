f7234079d5bdacf4a7e297d5870c12afe209c2ac
toybat <applicationstoybatbello@gmail.com>
2023-07-21 22:47:57 +0100

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/*.{html,js}'],
+  theme: {
+    extend: {
+      colors: {
+        bleu: 'hsla(206, 100%, 48%, 1)',
+        unitedBlue: 'hsla(211, 66%, 58%, 1)',
+        beaver: 'hsla(20, 21%, 54%, 1)',
+        ghostWhite: 'hsla(222, 31%, 94%, 1)',
+        licorice: 'hsla(352, 52%, 8%, 1)',
+        coffee: 'hsla(22, 38%, 28%, 1)'
+      
+      },
+    },
+
+  },
+  plugins: [],
+}
+
